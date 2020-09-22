28cdcba2b341724893f3e04bba94a572ebcd79d1
Gacek <szymon.gackowski@callpage.io>
2020-09-22 19:46:56 +0200

    cleanup

--- auto_stack.py
+++ auto_stack.py
@@ -4,7 +4,6 @@ import numpy as np
 from time import time
 
 
-
 # Align and stack images with ECC method
 # Slower but more accurate
 def stackImagesECC(file_list):
@@ -14,29 +13,28 @@ def stackImagesECC(file_list):
     stacked_image = None
 
     for file in file_list:
-        image = cv2.imread(file,1).astype(np.float32) / 255
+        image = cv2.imread(file, 1).astype(np.float32) / 255
         print(file)
         if first_image is None:
             # convert to gray scale floating point image
-            first_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+            first_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             stacked_image = image
         else:
             # Estimate perspective transform
-            s, M = cv2.findTransformECC(cv2.cvtColor(image,cv2.COLOR_BGR2GRAY), first_image, M, cv2.MOTION_HOMOGRAPHY)
+            s, M = cv2.findTransformECC(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), first_image, M, cv2.MOTION_HOMOGRAPHY)
             w, h, _ = image.shape
             # Align image to first image
             image = cv2.warpPerspective(image, M, (h, w))
             stacked_image += image
 
     stacked_image /= len(file_list)
-    stacked_image = (stacked_image*255).astype(np.uint8)
+    stacked_image = (stacked_image * 255).astype(np.uint8)
     return stacked_image
 
 
 # Align and stack images by matching ORB keypoints
 # Faster but less accurate
 def stackImagesKeypointMatching(file_list):
-
     orb = cv2.ORB_create()
 
     # disable OpenCL to because of bug in ORB in OpenCV 3.1
@@ -48,7 +46,7 @@ def stackImagesKeypointMatching(file_list):
     first_des = None
     for file in file_list:
         print(file)
-        image = cv2.imread(file,1)
+        image = cv2.imread(file, 1)
         imageF = image.astype(np.float32) / 255
 
         # compute the descriptors with ORB
@@ -65,7 +63,7 @@ def stackImagesKeypointMatching(file_list):
             first_kp = kp
             first_des = des
         else:
-             # Find matches and sort them in the order of their distance
+            # Find matches and sort them in the order of their distance
             matches = matcher.match(first_des, des)
             matches = sorted(matches, key=lambda x: x.distance)
 
@@ -80,14 +78,18 @@ def stackImagesKeypointMatching(file_list):
             imageF = cv2.warpPerspective(imageF, M, (h, w))
             stacked_image += imageF
 
-    stacked_image /= len(file_list)
-    stacked_image = (stacked_image*255).astype(np.uint8)
+    stacked_image /= (len(file_list) / 2)
+
+    stacked_image[stacked_image > 1.0] = 1
+
+    stacked_image = (stacked_image * (255/stacked_image.max())) #.astype(np.uint8)
+
     return stacked_image
+
 
 # ===== MAIN =====
 # Read all files in directory
 import argparse
-
 
 if __name__ == '__main__':
 
@@ -95,7 +97,7 @@ if __name__ == '__main__':
     parser.add_argument('input_dir', help='Input directory of images ()')
     parser.add_argument('output_image', help='Output image name')
     parser.add_argument('--method', help='Stacking method ORB (faster) or ECC (more precise)')
-    parser.add_argument('--show', help='Show result image',action='store_true')
+    parser.add_argument('--show', help='Show result image', action='store_true')
     args = parser.parse_args()
 
     image_folder = args.input_dir
@@ -110,7 +112,7 @@ if __name__ == '__main__':
     if args.method is not None:
         method = str(args.method)
     else:
-        method = 'KP'
+        method = 'ORB'
 
     tic = time()
 
@@ -121,7 +123,7 @@ if __name__ == '__main__':
         stacked_image = stackImagesECC(file_list)
 
     elif method == 'ORB':
-        #Stack images using ORB keypoint method
+        # Stack images using ORB keypoint method
         description = "Stacking images using ORB method"
         print(description)
         stacked_image = stackImagesKeypointMatching(file_list)
@@ -130,10 +132,10 @@ if __name__ == '__main__':
         print("ERROR: method {} not found!".format(method))
         exit()
 
-    print("Stacked {0} in {1} seconds".format(len(file_list), (time()-tic) ))
+    print("Stacked {0} in {1} seconds".format(len(file_list), (time() - tic)))
 
     print("Saved {}".format(args.output_image))
-    cv2.imwrite(str(args.output_image),stacked_image)
+    cv2.imwrite(str(args.output_image), stacked_image)
 
     # Show image
     if args.show:
